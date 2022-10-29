9ddff56136e6f4376ebce389b52f9d5a80c1d027
MattiaMarotta <92986713+MattiaMarotta@users.noreply.github.com>
2022-10-29 11:02:56 +0200

    Update grande.sh

--- grande.sh
+++ grande.sh
@@ -1,9 +1,21 @@
-a=$1
-b=$2
-file=$ciao
-if [ $a -gt $b ]
+NUM1=$1
+NUM2=$2
+FILENAME=$3
+
+echo "Numero 1: $NUM1"
+echo "Numero 2: $NUM2"
+echo "Numero 3: $NUM3"
+if [ $NUM1 -gt $NUM2 ]
 then 
-    cat $file
+    echo "il numero 1 è più grande del 2"
+    if [ -e $FILENAME ]
+    then
+    echo "Stampa il contenuto del file"
+    cat $FILENAME
+    else 
+    echo "il file non esiste, mostro i file con permessi della cartella corrente"
+    ls -l
+    fi
 else 
-    echo "errore"
+    echo "errore: il numero 1 è più piccolo del 2"
 fi
